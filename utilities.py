1b3341a7936576d15889954d3018db2ac6f795ba
suvrat <suvrat@suvrat.(none)>
2014-08-13 15:01:00 +0530

    removed tolist from features

--- utilities.py
+++ utilities.py
@@ -194,14 +194,14 @@ def prepDataTrain(data, label = 'Label', fields = [], split = True, splitPercent
 
   if split:
     if fillna:
-      return data, test, features.tolist(), fillVal
+      return data, test, features, fillVal
     else:
-      return data, test, features.tolist(), {}
+      return data, test, features, {}
   else:
     if fillna:
-      return data, [], features.tolist(), fillVal
+      return data, [], features, fillVal
     else:
-      return data, [], features.tolist(), {}
+      return data, [], features, {}
   
 
 def prepDataTest(data, fields = [], fillna = True, fillVal = {}, dump = False, dumpPrefix = 'set'):
@@ -226,6 +226,6 @@ def prepDataTest(data, fields = [], fillna = True, fillVal = {}, dump = False, d
 
 #dumpSmoothies("/mnt/crit/train.csv", "smoothies", ['C3', 'C4', 'C10', 'C12', 'C16', 'C21','C24', 'C26'])
 
-dumpSmoothies("/mnt/crit/train.csv", "ssmoothies", ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11', 'C12', 'C13', 'C14', 'C15', 'C16', 'C17', 'C18', 'C19', 'C20', 'C21', 'C22', 'C23', 'C24', 'C25', 'C26'])
+#dumpSmoothies("/mnt/crit/train.csv", "ssmoothies", ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11', 'C12', 'C13', 'C14', 'C15', 'C16', 'C17', 'C18', 'C19', 'C20', 'C21', 'C22', 'C23', 'C24', 'C25', 'C26'])
 
 #csvToSmoothedCSV('/mnt/crit/test.csv', '/mnt/crit/testSmooth.csv',['I1','I2','I3','I4','I5','I6','I7','I8','I9','I10','I11','I12','I13', 'Id', 'Label'], ['C1','C2','C3','C4','C5','C6','C7','C8','C9','C10','C11','C12','C13','C14','C15','C16','C17','C18','C19','C20','C21','C22','C23','C24','C25','C26'], smoothingPrefix = 'smoothies')
