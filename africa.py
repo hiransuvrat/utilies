df6adbe34ff0d337633c0c8999ffd7a8f9511122
suvrat <hiran.suvrat@gmail.com>
2014-09-23 00:19:14 +0530

    Validation score .61. LB score .42222

--- africa.py
+++ africa.py
@@ -9,6 +9,51 @@ import sklearn.feature_selection as fs
 import sklearn.svm as svm
 import analysis as an
 import matplotlib.pyplot as plt
+import sklearn.cluster as kmeans
+from sklearn import preprocessing
+import pandas as pd
+from numpy.lib import recfunctions
+
+def clusterData(data, fields, removeFeatures= True, enc = 0, clust = 0, isTrain = True, numberCluster = 20):
+    #Scale the data
+    min_max_scaler = preprocessing.MinMaxScaler()
+    scaledData = min_max_scaler.fit_transform(data[fields].tolist())
+    #scaler = preprocessing.StandardScaler().fit(data[fields].tolist())
+    #scaledData = scaler.transform(data[fields].tolist())
+
+    #If training then fit the data first
+    if isTrain:
+        clust = kmeans.KMeans(n_clusters = numberCluster)
+        clust.fit(scaledData)
+        
+    #Get predicted clusters
+    getCluster = clust.predict(scaledData)
+    
+    blankArr = np.zeros((len(getCluster),1))
+    blankArr[:, 0] = getCluster
+    
+    if isTrain:
+        enc = preprocessing.OneHotEncoder()
+        enc.fit(blankArr)
+
+    #Do dummy encoding
+    cluster = enc.transform(blankArr).toarray()
+
+    #Give names to the cluster
+    newColumns = ['1'] * len(cluster[0])
+    for i in range(len(cluster[0])):
+        newColumns[i] = 'cluster_' + str(i)
+        data = recfunctions.append_fields(data, newColumns[i], cluster[:, i])
+    
+    if removeFeatures:
+        names = []
+        for field in fields:
+            names = list(data.dtype.names)
+            if field in names:
+                names.remove(field)
+        data = data[names]
+
+    return data, clust, enc, newColumns
 
 headersTest = np.genfromtxt(sys.argv[2], delimiter=',', names=True)
 
@@ -18,7 +63,8 @@ pickTrain = ['BSAN','BSAS','BSAV','CTI','ELEV','EVI','LSTD','LSTN','REF1','REF2'
 data = np.genfromtxt(sys.argv[1], names=True, delimiter=',') #, usecols=(pickTrain))
 
 #Column to be picked from test file
-pickTest = ['PIDN', 'BSAN','BSAS','BSAV','CTI','ELEV','EVI','LSTD','LSTN','REF1','REF2','REF3','REF7','RELI','TMAP','TMFI']
+#pickTest = ['BSAN','BSAS','BSAV','CTI','ELEV','EVI','LSTD','LSTN','REF1','REF2','REF3','REF7','RELI','TMAP','TMFI']
+pickTest = ['BSAN','BSAS','BSAV','CTI','ELEV','EVI','LSTD','LSTN','REF1','REF2','REF3','REF7','RELI','TMAP','TMFI']
 test = np.genfromtxt(sys.argv[2], names=True, delimiter=',')#, usecols=(pickTest))
 
 ids = np.genfromtxt(sys.argv[2], dtype=str, skip_header=1, delimiter=',', usecols=0)
@@ -27,9 +73,10 @@ ids = np.genfromtxt(sys.argv[2], dtype=str, skip_header=1, delimiter=',', usecol
 #featuresList = ['BSAN','BSAS','BSAV','CTI','ELEV','EVI','LSTD','LSTN','REF1','REF2','REF3','REF7','RELI','TMAP','TMFI']
 featuresList = np.array(headersTest.dtype.names)
 featuresList = featuresList[2:-5]
-
+clusterFields = ['TMAP']
 #Keep a copy of train file for later use
 data1 = np.copy(data)
+test1 = np.copy(test)
 
 #Dependent/Target variables
 targets = ['Ca','P','pH','SOC','Sand']
@@ -38,12 +85,13 @@ targets = ['Ca','P','pH','SOC','Sand']
 #Prepare empty result
 df = pd.DataFrame({"PIDN": ids, "Ca": test['PIDN'], "P": test['PIDN'], "pH": test['PIDN'], "SOC": test['PIDN'], "Sand": test['PIDN']})
 testa = 0
-
+             
 for target in targets:
     data = np.copy(data1)
-    bs = cross_validation.Bootstrap(len(data), random_state=0)
-
+    test = np.copy(test1)
+    #bs = cross_validation.Bootstrap(len(data), random_state=0)
     
+             
     #Prepare data for training
     
     delList = np.array([])
@@ -54,13 +102,18 @@ for target in targets:
     for i in range(len(data)):
         if data[target][i] > (data[target].mean() + 2*data[target].std()) or data[target][i] < (data[target].mean() - 2*data[target].std()):
             delList = np.append(delList, i)
+            print (data[target].mean() - 1*data[target].std()), data[target].std()
     '''
+
     #clf = linear.BayesianRidge(normalize=True, verbose=True, tol=.01)
-    clf = svm.SVR(C=10000.0)
+    clf = svm.SVR(C=10000.0, kernel='rbf', degree=1)
     data = np.delete(data, delList, 0)
-    data, testa, features, fillVal = util.prepDataTrain(data, target, featuresList, True, 20, True, True, 'mean', False, 'set')
+    data, testa, features, fillVal = util.prepDataTrain(data, target, featuresList, False, 30, False, True, 'mean', False, 'set')
+    #data, clust, enc, newCol = clusterData(data, clusterFields, True)
+    #testa, clust, enc, newCol = clusterData(testa, pickTest, True, enc, clust, False)
+    #features = np.concatenate((features, newCol))
+    
     #sel = fs.SelectKBest(fs.f_regression, k=2000)
-    print data.shape
     #data = np.array(sel.fit_transform(data[features].tolist(), data1[target]))
     #print data.shape
 
@@ -73,17 +126,20 @@ for target in targets:
     clf.fit(data[features].tolist(), data[target])
 
     #Prepare test data
-    #test = util.prepDataTest(test, featuresList, True, fillVal, False, 'set')
-    
+    #testa, clust, enc, newCol = clusterData(testa, clusterFields, True, enc, clust, False)
+    test = util.prepDataTest(test, features, True, fillVal, False, 'set')
+
     #Get predictions
-    #pred = clf.predict(test[features].tolist())
-    pred1 = clf.predict(testa[features].tolist())
-    pred = clf.predict(data[features].tolist())
+    pred = clf.predict(test[features].tolist())
+    #pred1 = clf.predict(testa[features].tolist())
+    #pred = clf.predict(data[features].tolist())
     #an.plotData(data[target], pred)
 
     #Store results
-    #df[target] = pred
-    #testa[target] = (testa[target] - pred) ** 2
+    df[target] = pred
+    continue
+
+    print (((((testa[target] - pred1) ** 2).sum())/len(pred1))**.5)
     abse = pred - data[target]
     absp = pred1 - testa[target]
     abse = abse[abse < 1]
@@ -95,7 +151,7 @@ for target in targets:
     plt.hist(absp, bins=100, color='red')
     plt.show()
 
-np.savetxt("testActual.csv", testa, delimiter=",", fmt='%.4e')
-#df.to_csv("predictions.csv", index=False, cols=["PIDN","Ca","P","pH","SOC","Sand"])
+#np.savetxt("testActual.csv", testa, delimiter=",", fmt='%.4e')
+df.to_csv("predictions.csv", index=False, cols=["PIDN","Ca","P","pH","SOC","Sand"])
 
 
